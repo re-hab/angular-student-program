822a0df923f319e0f986701866234e37a6631f86
Rehab <rahmed@panarab-media.com>
2021-03-09 02:32:17 +0200

    refactor: do some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ const routes: Routes = [
     // loadChildren: () => 
     // import('./pages/home/home.module').then((m) => m.HomeModule),
      pathMatch: 'full',
-     
   },
   {
     path: 'programs',
