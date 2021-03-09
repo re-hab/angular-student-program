7e011c0702a03f44db190fd153d3f7c675975997
Rehab <rahmed@panarab-media.com>
2021-03-09 14:15:56 +0200

    refactors: change scroll bar and remove console and some code for test

--- src/app/pages/programs/components/list/list.component.ts
+++ src/app/pages/programs/components/list/list.component.ts
@@ -48,19 +48,6 @@ export class ListComponent implements OnInit {
   getPrograms() {
     this.progService.getPrograms().subscribe((data: any) => {
       this.programs = data[2].data
-
-      let flags = [];
-      let output = [];
-      let l = this.programs.length
-      
-      for (let i = 0; i < l; i++) {
-        if (flags[this.programs[i].type]) continue;
-        flags[this.programs[i].type] = true;
-        output.push(this.programs[i].type);
-      }
-
-      console.log(flags)
-
       this.filter()
     })
   }
